b9a1329008141923fbca75092b18a5b919a0f0d4
Eric Armijo <rcrmj@hotmail.com>
2023-07-03 10:13:26 -0400

    Update generar_informe_mespasado.R

--- generar_informe_mespasado.R
+++ generar_informe_mespasado.R
@@ -3,7 +3,9 @@
 #  (Reportes de Adscripción)                         
 #  filtrando para dejar los registros correspondientes
 #  al municipio de Santa Cruz de la Sierra, para  
-#  el mes pasado                                     
+#  el mes pasado     
+#  Requiere descargar previamente el archivo SUS
+#  (MS Excel) del sitio web https://sus.minsalud.gob.bo/loginS
 #                                    
 #  Desarrollado por:                 
 #  Eric Armijo  (rcrmj@hotmail.com)  
@@ -31,10 +33,6 @@ if (!require(writexl)) {
   library(writexl)
 }
 
-# Establece la carpeta de trabajo
-work_dir <- "D:/Usuarios/earmijo/Documents/sist/SUS/informes/06"
-setwd(work_dir)
-
 # Obtiene la lista de todos los archivos Excel en el directorio
 file_paths <- list.files(pattern = "\\.xlsx$")
 
@@ -43,20 +41,11 @@ for (file_path in file_paths) {
   # Lee la primera hoja del archivo de Excel en un data.frame, saltando la primera fila
   data <- read_excel(file_path, skip = 1)
   
-  # Calcula el año y el mes actual
-  current_year <- as.integer(format(Sys.Date(), "%Y"))
-  last_month <- month(Sys.Date()) - 1
-  if (last_month == 0) {
-    last_month <- 12
-    current_year <- current_year - 1
-  }
   
   # Filtra las filas donde el campo "Municipio" es igual a "SANTA CRUZ DE LA SIERRA",
   # y el campo "Gestion" es igual al año actual, y el campo "Mes" es igual al mes pasado
   data <- filter(data, Municipio == "SANTA CRUZ DE LA SIERRA", Gestion == current_year, Mes == last_month)
   
-  # Forma el nombre del archivo final
-  file_name <- paste0("SUS-ConsolidadoAdscripcion-municScz_", current_year, "-", sprintf("%02d", last_month), ".xlsx")
   
   # Guarda el data.frame resultante en un nuevo archivo de Excel
   write_xlsx(data, file_name)
