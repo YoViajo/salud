b6e229ed39c0f97d0b0e81590ca5e897d74b764c
Eric Armijo <rcrmj@hotmail.com>
2023-07-03 09:31:02 -0400

    Adic scripts R

---
+++ enviar_correoe.R
@@ -0,0 +1,40 @@
+######################################
+#  Envía un mensaje por Outlook para 
+#  anunciar la disponibilidad del    
+#  Informe de adscripción SUS del    
+#  mes pasado                        
+#  Requiere tener iniciada una
+#  sesión de MS Outlook
+#                                    
+#  Desarrollado por:                 
+#  Eric Armijo  (rcrmj@hotmail.com)  
+######################################
+
+
+# Instala y carga las bibliotecas necesarias.
+# install.packages("RDCOMClient", repos = "http://www.omegahat.net/R")
+# install.packages("lubridate")
+library(RDCOMClient)
+library(lubridate)
+
+# Establece la localidad en español.
+Sys.setlocale("LC_TIME", "Spanish")
+
+# Obtén el mes pasado y el año actual.
+fecha_pasada <- Sys.Date() %m-% months(1)
+mes_pasado <- format(fecha_pasada, "%B")
+ano_actual <- year(fecha_pasada)
+
+# Crea un objeto de Outlook.
+OutApp <- COMCreate("Outlook.Application")
+
+# Crea un nuevo correo electrónico.
+OutMail = OutApp$CreateItem(0)
+
+# Configura los detalles del correo electrónico.
+OutMail[["To"]] = "szcadima@gmsantacruz.gob.bo"
+OutMail[["Subject"]] = paste("Informe de adscripción SUS para el municipio de Santa Cruz – ", mes_pasado, ano_actual, sep = " ")
+OutMail[["Body"]] = paste("Estimada Ing.,\n\nYa está disponible el “Informe de adscripción SUS para el municipio de Santa Cruz – ", mes_pasado, ano_actual, "”. El archivo correspondiente, obtenido a partir de datos proporcionados por el sitio web SUS, ha sido copiado al directorio compartido: http://t.ly/LUQ_F\n\nAtte.")
+
+# Envía el correo electrónico.
+OutMail$Send()
---
+++ generar_informe_mespasado.R
@@ -0,0 +1,65 @@
+######################################################
+#  Procesa el archivo generado por el sistema SUS    
+#  (Reportes de Adscripción)                         
+#  filtrando para dejar los registros correspondientes
+#  al municipio de Santa Cruz de la Sierra, para el  
+#  el mes pasado                                     
+#                                    
+#  Desarrollado por:                 
+#  Eric Armijo  (rcrmj@hotmail.com)  
+######################################################
+
+
+# Instala y carga los paquetes readxl, dplyr, lubridate y writexl si aún no lo has hecho
+if (!require(readxl)) {
+  install.packages("readxl")
+  library(readxl)
+}
+
+if (!require(dplyr)) {
+  install.packages("dplyr")
+  library(dplyr)
+}
+
+if (!require(lubridate)) {
+  install.packages("lubridate")
+  library(lubridate)
+}
+
+if (!require(writexl)) {
+  install.packages("writexl")
+  library(writexl)
+}
+
+# Establece la carpeta de trabajo
+work_dir <- "D:/Usuarios/earmijo/Documents/sist/SUS/informes/06"
+setwd(work_dir)
+
+# Obtiene la lista de todos los archivos Excel en el directorio
+file_paths <- list.files(pattern = "\\.xlsx$")
+
+# Itera sobre cada archivo
+for (file_path in file_paths) {
+  # Lee la primera hoja del archivo de Excel en un data.frame, saltando la primera fila
+  data <- read_excel(file_path, skip = 1)
+  
+  # Calcula el año y el mes actual
+  current_year <- as.integer(format(Sys.Date(), "%Y"))
+  last_month <- month(Sys.Date()) - 1
+  if (last_month == 0) {
+    last_month <- 12
+    current_year <- current_year - 1
+  }
+  
+  # Filtra las filas donde el campo "Municipio" es igual a "SANTA CRUZ DE LA SIERRA",
+  # y el campo "Gestion" es igual al año actual, y el campo "Mes" es igual al mes pasado
+  data <- filter(data, Municipio == "SANTA CRUZ DE LA SIERRA", Gestion == current_year, Mes == last_month)
+  
+  # Forma el nombre del archivo final
+  file_name <- paste0("SUS-ConsolidadoAdscripcion-municScz_", current_year, "-", sprintf("%02d", last_month), ".xlsx")
+  
+  # Guarda el data.frame resultante en un nuevo archivo de Excel
+  write_xlsx(data, file_name)
+}
+
+
